8f1d12bd507fa6f9ff66fdbd5dc0a446c60cfef2
Logan Gray <loganallengray@gmail.com>
2022-12-12 09:51:37 -0600

    finished practice

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (num) => {
+    const quarteredNum = num / 4;
+
+    return quarteredNum;
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(4);
+
+// Log the return value
+console.log(result);
